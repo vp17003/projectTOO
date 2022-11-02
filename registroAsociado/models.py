cd2a50aff88d77ddce7e1a94344c8a2ac4b2a25a
CarlosRiveraUES <rr16121@ues.edu.sv>
2022-11-01 23:32:08 -0600

    modelos de asociacion

--- registroAsociado/models.py
+++ registroAsociado/models.py
@@ -1,8 +1,28 @@
+from dataclasses import asdict
 from django.db import models
 
 
 # Create your models here.
-class s_financiero(models.Model):
+class ejecutivo(models.Model):
     idEjecutivo = models.AutoField(primary_key=True)
     nombreEjecutivo = models.CharField(max_length=50)
     apellidoEjecutivo = models.CharField(max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'ejecutivo'
+    def __str__(self):
+        return f'{self.idEjecutivo}'
+
+# Create your models here.
+class asociacion(models.Model):
+    idAsociado = models.AutoField(primary_key=True)
+    fechaAsociacion = models.CharField(max_length=50)
+    lugarAsociacion = models.CharField(max_length=100)
+    ejecutivo_idEjecutivo= models.ForeignKey(ejecutivo, on_delete=models.CASCADE)
+
+    class Meta:
+        managed = True
+        db_table = 'asociacion'
+    def __str__(self):
+        return f'{self.idAsociado}'
