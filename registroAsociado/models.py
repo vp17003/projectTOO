965cf9ed7e9940001aa51f914d6ea0de691dd32c
vp17003 <89763953+vp17003@users.noreply.github.com>
2022-11-02 14:45:57 -0600

    Erick

--- registroAsociado/models.py
+++ registroAsociado/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 # Create your models here.
+# Tabla ejecutico
 class ejecutivo(models.Model):
     idEjecutivo = models.AutoField(primary_key=True)
     nombreEjecutivo = models.CharField(max_length=50)
@@ -15,6 +16,7 @@ class ejecutivo(models.Model):
         return f'{self.idEjecutivo}'
 
 # Create your models here.
+#Tabla asociacion
 class asociacion(models.Model):
     idAsociado = models.AutoField(primary_key=True)
     fechaAsociacion = models.CharField(max_length=50)
@@ -26,3 +28,41 @@ class asociacion(models.Model):
         db_table = 'asociacion'
     def __str__(self):
         return f'{self.idAsociado}'
+
+#Tabla genero
+class genero(models.Model):
+    idGenero = models.AutoField(primary_key=True)
+    # Por sno funciona lo de abajo esta la linea 36
+    # nombreGenero = models.CharField(max_length=8, unique=True)
+    GENERO = (
+        ('H', 'HOMBRE'),
+        ('M', 'MUJER'),
+    )
+    estadoCuenta = models.CharField(max_length=1, choices=GENERO)
+        
+    class Meta:
+        managed = True
+        db_table = 'genero'
+    def __str__(self):
+        return f'{self.idGenero}'
+
+class tipoDocumento(models.Model):
+    idTipoDocumento = models.AutoField(primary_key=True)
+    # Por sno funciona lo de abajo esta la linea 52
+    # nombreGDocumento = models.CharField(max_length=10, unique=True)
+    DOCUMENTO = (
+        ('DUI', 'DUI'),
+        ('NIT', 'NIT'),
+        ('PARTI','PARTIDA DE NACIMIENTO'),
+        ('PASAP','PASAPORTE'),
+        ('CARNE','CARNE DE RESIDENCIA'),
+    )
+    nombreDocumento = models.CharField(max_length=5, choices=DOCUMENTO, default='DUI')
+        
+    class Meta:
+        managed = True
+        db_table = 'tipoDocumento'
+    def __str__(self):
+        return f'{self.idTipoDocumento}'
+
+    
